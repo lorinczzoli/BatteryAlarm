df22199fdb68cc0f1c9fde4769b09968e1febb41
LorinczZoli <lorinczzoli@yahoo.com>
2018-02-08 21:59:51 +0200

    BatteryAlarm - 1.2.0.66 -add: snooze period is configurable on the activity -add: snooze time is loaded/saved in shared preferences -note: this value is not yet used

--- app/src/main/java/com/yahoo/lorinczzoli/batteryalarm/MainActivity.java
+++ app/src/main/java/com/yahoo/lorinczzoli/batteryalarm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yahoo.lorinczzoli.batteryalarm;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -13,6 +14,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,14 +23,16 @@ public class MainActivity extends AppCompatActivity {
     public static final String KEY_CHARGER_DISCONNECTED_ALARM_ENABLED = "KEY_CHARGER_DISCONNECTED_ALARM_ENABLED";
     public static final String KEY_SOUND_TYPE = "KEY_SOUND_TYPE";
     public static final String KEY_SNOOZE_ACTIVE = "KEY_SNOOZE_ACTIVE";
+    public static final String KEY_SNOOZE_SEC = "KEY_SNOOZE_SEC";
     public static final String PREF_NAME = "Example01";
 
     private static Ringtone r = null;
 
-    CheckBox chkLowBattery;
-    CheckBox chkChargerDisconnected;
+    CheckBox chkLowBattery, chkChargerDisconnected, chkSnooze;
     Button btnStopAlarmSound;
     RadioButton rdoRingtone, rdoAlarm;
+    EditText edtSnooze;
+    TextView txtSnooze;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         chkLowBattery = findViewById(R.id.chkLowBattery);
         chkChargerDisconnected = findViewById(R.id.chkChargerDisconnected);
+
+        chkSnooze = findViewById(R.id.chkSnooze);
+        edtSnooze = findViewById(R.id.edtSnooze);
+        txtSnooze = findViewById(R.id.txtSnooze);
+
         btnStopAlarmSound = findViewById(R.id.btnStopAlarmSound);
 
         rdoRingtone = findViewById(R.id.rdoRingtone);
@@ -61,6 +71,19 @@ public class MainActivity extends AppCompatActivity {
 
         bEnabled = sp.getBoolean(KEY_SNOOZE_ACTIVE, false);
         btnStopAlarmSound.setEnabled(MainReceiver.IsRinging() || bEnabled);
+
+        int nSnoozeSec = sp.getInt(KEY_SNOOZE_SEC, 0);
+        Log.e("BATT", Integer.toString(nSnoozeSec) );
+
+        if (nSnoozeSec > 0) chkSnooze.setChecked(true);
+        else
+        {
+            edtSnooze.setEnabled(false);
+            txtSnooze.setEnabled(false);
+        }
+
+        nSnoozeSec = Math.abs(nSnoozeSec);
+        edtSnooze.setText( Integer.toString(nSnoozeSec) );
     }
 
     public void onClick(View v) {
@@ -81,6 +104,31 @@ public class MainActivity extends AppCompatActivity {
 
                 bIsChecked = chkChargerDisconnected.isChecked();
                 spe.putBoolean(KEY_CHARGER_DISCONNECTED_ALARM_ENABLED, bIsChecked);
+
+                spe.commit();
+
+                break;
+            case R.id.chkSnooze:
+                bIsChecked = chkSnooze.isChecked();
+                edtSnooze.setEnabled(bIsChecked);
+                txtSnooze.setEnabled(bIsChecked);
+
+                if (bIsChecked) txtSnooze.requestFocus();
+
+                int nSnoozeSec = 0;
+                String sSnoozeSec = edtSnooze.getText().toString();
+
+                Log.e("BATT", "sSnoozeSec = '" + sSnoozeSec + "'");
+                if (sSnoozeSec.isEmpty() == false) nSnoozeSec = Integer.valueOf(sSnoozeSec);
+                if (bIsChecked == false) nSnoozeSec *= -1;
+
+                Log.e("BATT", nSnoozeSec + " sec");
+
+                sp = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+                spe = sp.edit();
+
+                bIsChecked = chkChargerDisconnected.isChecked();
+                spe.putInt(KEY_SNOOZE_SEC, nSnoozeSec);
 
                 spe.commit();
 
